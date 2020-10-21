a1b35265e425850e4581e3692a10b1cacf0ab621
Jakob Christiansen <jch@wexo.dk>
2020-10-21 08:49:49 +0200

    [HK-58]Deleting cart on finalize payment

---
+++ src/Core/Checkout/Cart/SalesChannel/CartOrderRouteDecorator.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Wexo\Quickpay\Core\Checkout\Cart\SalesChannel;
+
+use Shopware\Core\Checkout\Cart\Cart;
+use Shopware\Core\Checkout\Cart\CartCalculator;
+use Shopware\Core\Checkout\Cart\CartPersisterInterface;
+use Shopware\Core\Checkout\Cart\Event\CheckoutOrderPlacedEvent;
+use Shopware\Core\Checkout\Cart\Order\OrderPersisterInterface;
+use Shopware\Core\Checkout\Cart\SalesChannel\AbstractCartOrderRoute;
+use Shopware\Core\Checkout\Cart\SalesChannel\CartOrderRouteResponse;
+use Shopware\Core\Checkout\Order\Aggregate\OrderCustomer\OrderCustomerEntity;
+use Shopware\Core\Checkout\Order\OrderEntity;
+use Shopware\Core\Checkout\Payment\Exception\InvalidOrderException;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+class CartOrderRouteDecorator extends AbstractCartOrderRoute
+{
+    /**
+     * @var \Shopware\Core\Checkout\Cart\SalesChannel\CartOrderRoute
+     */
+    protected $decoratedService;
+    /**
+     * @var CartCalculator
+     */
+    protected $cartCalculator;
+
+    /**
+     * @var EntityRepositoryInterface
+     */
+    protected $orderRepository;
+
+    /**
+     * @var OrderPersisterInterface
+     */
+    protected $orderPersister;
+
+    /**
+     * @var CartPersisterInterface
+     */
+    protected $cartPersister;
+
+    /**
+     * @var EventDispatcherInterface
+     */
+    protected $eventDispatcher;
+
+    /**
+     * @var EntityRepositoryInterface
+     */
+    protected $orderCustomerRepository;
+
+    public function __construct(
+        \Shopware\Core\Checkout\Cart\SalesChannel\CartOrderRoute $cartOrderRoute,
+        CartCalculator $cartCalculator,
+        EntityRepositoryInterface $orderRepository,
+        EntityRepositoryInterface $orderCustomerRepository,
+        OrderPersisterInterface $orderPersister,
+        CartPersisterInterface $cartPersister,
+        EventDispatcherInterface $eventDispatcher
+    )
+    {
+        $this->decoratedService = $cartOrderRoute;
+        $this->cartCalculator = $cartCalculator;
+        $this->orderRepository = $orderRepository;
+        $this->orderCustomerRepository = $orderCustomerRepository;
+        $this->orderPersister = $orderPersister;
+        $this->cartPersister = $cartPersister;
+        $this->eventDispatcher = $eventDispatcher;
+    }
+
+    public function getDecorated(): AbstractCartOrderRoute
+    {
+        return $this->decoratedService->getDecorated();
+    }
+
+    public function order(Cart $cart, SalesChannelContext $context): CartOrderRouteResponse
+    {
+        $calculatedCart = $this->cartCalculator->calculate($cart, $context);
+        $orderId = $this->orderPersister->persist($calculatedCart, $context);
+
+        $criteria = new Criteria([$orderId]);
+        $criteria
+            ->addAssociation('deliveries.shippingMethod')
+            ->addAssociation('deliveries.shippingOrderAddress.country')
+            ->addAssociation('transactions.paymentMethod')
+            ->addAssociation('lineItems')
+            ->addAssociation('currency')
+            ->addAssociation('addresses.country');
+
+        /** @var OrderEntity|null $orderEntity */
+        $orderEntity = $this->orderRepository->search($criteria, $context->getContext())->first();
+
+        if (!$orderEntity) {
+            throw new InvalidOrderException($orderId);
+        }
+
+        $orderEntity->setOrderCustomer(
+            $this->fetchCustomer($orderEntity->getId(), $context->getContext())
+        );
+
+        $orderPlacedEvent = new CheckoutOrderPlacedEvent(
+            $context->getContext(),
+            $orderEntity,
+            $context->getSalesChannel()->getId()
+        );
+
+        $this->eventDispatcher->dispatch($orderPlacedEvent);
+
+        return new CartOrderRouteResponse($orderEntity);
+    }
+
+    private function fetchCustomer(string $orderId, Context $context): OrderCustomerEntity
+    {
+        $criteria = new Criteria();
+        $criteria->addFilter(new EqualsFilter('orderId', $orderId));
+        $criteria->addAssociation('customer');
+        $criteria->addAssociation('salutation');
+
+        return $this->orderCustomerRepository
+            ->search($criteria, $context)
+            ->first();
+    }
+}
--- src/Service/QuickpayPayment.php
+++ src/Service/QuickpayPayment.php
@@ -5,6 +5,7 @@ namespace Wexo\Quickpay\Service;
 use Exception;
 use GuzzleHttp\Client;
 use Monolog\Logger;
+use Shopware\Core\Checkout\Cart\CartPersisterInterface;
 use Shopware\Core\Checkout\Order\Aggregate\OrderLineItem\OrderLineItemCollection;
 use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionStateHandler;
 use Shopware\Core\Checkout\Order\SalesChannel\OrderService;
@@ -41,6 +42,8 @@ class QuickpayPayment implements AsynchronousPaymentHandlerInterface
     protected $logEntryRepository;
     /** @var Client $http */
     public $http;
+    /** @var CartPersisterInterface */
+    protected $cartPersister;
 
     /**
      * QuickpayPayment constructor.
@@ -55,13 +58,15 @@ class QuickpayPayment implements AsynchronousPaymentHandlerInterface
         EntityRepositoryInterface $logEntryRepository,
         EntityRepositoryInterface $orderRepository,
         OrderTransactionStateHandler $transactionStateHandler,
-        OrderService $orderService
+        OrderService $orderService,
+        CartPersisterInterface $cartPersister
     ) {
         $this->systemConfigService = $systemConfigService;
         $this->logEntryRepository = $logEntryRepository;
         $this->orderRepository = $orderRepository;
         $this->transactionStateHandler = $transactionStateHandler;
         $this->orderService = $orderService;
+        $this->cartPersister = $cartPersister;
 
         $this->http = new Client([
             'base_uri' => 'https://api.quickpay.net/',
@@ -177,6 +182,7 @@ class QuickpayPayment implements AsynchronousPaymentHandlerInterface
         $context = $salesChannelContext->getContext();
         if ($request->get('status') == "accepted") {
             // Payment completed
+            $this->cartPersister->delete($salesChannelContext->getToken(), $salesChannelContext);
             $this->transactionStateHandler->paid($transaction->getOrderTransaction()->getId(), $context);
         } elseif ($request->get('status') == "cancel") {
             throw new CustomerCanceledAsyncPaymentException(
