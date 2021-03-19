7afe82eb056795719c80590c209a07546066297d
Jonas Søndergaard <jonas@wexo.dk>
2021-03-19 12:41:57 +0100

    [SLP-299] PHPCS

--- src/Service/QuickpayPayment.php
+++ src/Service/QuickpayPayment.php
@@ -611,12 +611,8 @@ class QuickpayPayment implements AsynchronousPaymentHandlerInterface
                         $transaction->getId(),
                         $context
                     );
-                } elseif (
-                    !in_array(
-                        $stateName,
-                        [OrderTransactionStates::STATE_PAID, OrderTransactionStates::STATE_PARTIALLY_PAID,]
-                    )
-                ) {
+                } elseif ($stateName !== OrderTransactionStates::STATE_PAID &&
+                    $stateName !== OrderTransactionStates::STATE_PARTIALLY_PAID) {
                     $this->transactionStateHandler->payPartially(
                         $transaction->getId(),
                         $context
