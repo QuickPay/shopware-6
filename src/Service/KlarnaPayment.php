462223e8d9ffedc6d6942d09b6819cec9eec7b97
wexomsm <msm@wexo.dk>
2025-09-09 16:25:19 +0200

    [46557-458]reverted payment classes for codesniffer

--- src/Service/KlarnaPayment.php
+++ src/Service/KlarnaPayment.php
@@ -2,8 +2,32 @@
 
 namespace Wexo\Quickpay\Service;
 
+use GuzzleHttp\Exception\GuzzleException;
+use Shopware\Core\Checkout\Payment\Cart\PaymentTransactionStruct;
+use Shopware\Core\Framework\Struct\Struct;
+use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Shopware\Core\Framework\Context;
+use Symfony\Component\HttpFoundation\Request;
+
 class KlarnaPayment extends QuickpayPayment
 {
     public static string $quickpayName = 'klarna-payments';
 
+    /**
+     * @param Request $request
+     * @param PaymentTransactionStruct $transaction
+     * @param Context $context
+     * @param Struct|null $validateStruct
+     * @return RedirectResponse
+     */
+    public function pay(
+        Request $request,
+        PaymentTransactionStruct $transaction,
+        Context $context,
+        ?Struct $validateStruct
+    ): RedirectResponse {
+        return parent::pay($request, $transaction, $context, $validateStruct);
+    }
 }
--- src/Service/SwishPayment.php
+++ src/Service/SwishPayment.php
@@ -2,8 +2,33 @@
 
 namespace Wexo\Quickpay\Service;
 
+use GuzzleHttp\Exception\GuzzleException;
+use Shopware\Core\Checkout\Payment\Cart\AsyncPaymentTransactionStruct;
+use Shopware\Core\Checkout\Payment\Cart\PaymentTransactionStruct;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\Struct\Struct;
+use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+
 class SwishPayment extends QuickpayPayment
 {
     public static string $quickpayName = 'swish';
 
+    /**
+     * @param Request $request
+     * @param PaymentTransactionStruct $transaction
+     * @param Context $context
+     * @param Struct|null $validateStruct
+     * @return RedirectResponse
+     */
+    public function pay(
+        Request $request,
+        PaymentTransactionStruct $transaction,
+        Context $context,
+        ?Struct $validateStruct
+    ): RedirectResponse {
+        return parent::pay($request, $transaction, $context, $validateStruct);
+    }
 }
