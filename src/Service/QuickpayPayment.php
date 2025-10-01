26ea1ff4bf326d424594b3d213ff9ae140b02914
wexoak <ak@wexo.dk>
2025-10-01 13:53:50 +0200

    [46557-458] Remove redundant database query

--- src/Service/QuickpayPayment.php
+++ src/Service/QuickpayPayment.php
@@ -87,8 +87,6 @@ class QuickpayPayment extends AbstractPaymentHandler
         try {
             if (! isset($customFields[WexoQuickpay::QUICKPAY_RESPONSE_FIELD])) {
                 $this->currentService->create($transaction, $orderTransaction, $order, $context);
-                $orderTransaction = $this->loadTransaction($orderTransactionId, $context);
-                $order = $orderTransaction->getOrder();
             }
             $link = $this->currentService->getLink($transaction, $context, $orderTransaction, $order, $extraParams);
         } catch (Exception $e) {
